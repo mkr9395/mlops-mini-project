eb6c3b2cce3db2614935d6db240095e179416f66
Mohit Kumar <mkr9395@gmail.com>
2024-10-17 15:49:21 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='end 2 end mini project on mlops stack',
+    author='Mohit Kumar',
+    license='',
+)
